5e5922355093c541dd79328305d7bce8d78afa98
Harpreet Kaur <harpreetkaurguglani@gmail.com>
2022-03-08 20:11:54 -0800

    Changes

--- src/app.py
+++ src/app.py
@@ -16,6 +16,14 @@ data = pd.read_csv("../data/energydata_complete.csv")
 
 data["date"] = pd.to_datetime(data["date"])
 
+energy_data['month'] = energy_data['date'].dt.month
+energy_data_subset=energy_data[['Appliances','lights','date']]
+energy_data_subset['month_full'] = energy_data_subset['date'].dt.month_name()
+energy_data_subset=energy_data_subset.groupby('month_full', sort=False).sum().reset_index()
+energy_data_subset=pd.melt(energy_data_subset,id_vars =['month_full'], value_vars=['Appliances', 'lights'])
+energy_data_subset.head()
+sort_order = ['January', 'February','March','April','May']
+
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 ## Plotting
@@ -55,6 +63,19 @@ plot1 = html.Iframe(
     style={"width": "100%", "height": "1000px"},
 )
 
+def area_plot(start_date,end_date):
+    
+    selected_data = energydata[(energydata['day'] <= end_date) & (energydata['day'] >= start_date)]
+    
+    plot = alt.Chart(energy_data_subset).mark_area().encode(
+        x = alt.X('month_full', sort = sort_order, axis = alt.Axis(title = 'Month', tickCount = 10, grid = False, labelAngle = -360),
+        scale = alt.Scale(zero = False, domain = list(sort_order))),
+        y = alt.Y('value', axis = alt.Axis(title = 'Energy use in Wh', grid = False),scale = alt.Scale(zero = False) ),
+        color = alt.Color('variable')
+        ).properties(height = 300, width = 500, title = "Energy Used in house"
+        ).configure_axis(labelFontSize = 14, titleFontSize = 18) 
+
+    return plot.to_html()
 
 app.layout = dbc.Container(
     [
